7ea63848575af4f5bdc5b4b3c61ff3ab486fc91a
Animesh Jain <animesh.jain1203@gmail.com>
2025-10-17 12:42:29 +0200

    Cleaning up profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,27 +1,6 @@
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
 
-  def new
-  end
-
-  def edit
-  end
-
-  def update
-    if current_user.update(profile_params)
-      redirect_to view_profile_path(current_user), notice: "Profile updated successfully."
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  private
-  def profile_params
-    permitted = params.require(:user).permit(:name, :password, :password_confirmation)
-    if permitted[:password].blank?
-      permitted.delete(:password)
-      permitted.delete(:password_confirmation)
-    end
-    permitted
+  def show
   end
 end
